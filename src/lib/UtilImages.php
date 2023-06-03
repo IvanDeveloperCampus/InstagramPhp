4a5da7d5fb256a8b7df115230ed48a168d68c865
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-03 14:19:13 -0500

    docs: :memo: Comentarios para tener claro que hace cada linea

--- src/lib/UtilImages.php
+++ src/lib/UtilImages.php
@@ -6,32 +6,27 @@ class UtilImages{
 
     public static function storeImage($photo):string{
 
-        $target_dir = "public/img/photos/";
+        $target_dir = "public/img/photos/"; //RUTA DONDE SE VA ALMACENAR LA FOTO
         $extarr = explode('.',$photo["name"]);
-        $filename = $extarr[sizeof($extarr)-2];
-        $ext = $extarr[sizeof($extarr)-1];
-        $hash = md5(Date('Ymdgi') . $filename) . '.' . $ext;
-        $target_file = $target_dir . $hash;
-        $uploadOk = 1;
-        //$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        
-        $check = getimagesize($photo["tmp_name"]);
+        $filename = $extarr[sizeof($extarr)-2];//GUARDAR EL NOMBRE DEL ARHICVO SIN LA EXTENSION
+        $ext = $extarr[sizeof($extarr)-1];//SE GUARDA LA EXTENSION JPG 
+        $hash = md5(Date('Ymdgi') . $filename) . '.' . $ext; //SE CREA UN HASH UNICO GENERADO CON LA FECHA Y EL NOMBRE Y A LO ULTIMO SE AGREGA LA EXTENSUION
+        $target_file = $target_dir . $hash; //AQUI SE CONCATENA LA RUTA OBJETIVO Y EL HASH
+        $uploadOk = 1; //BANDERA
+        $check = getimagesize($photo["tmp_name"]);//PARA OBTENER INFORMACION EN EL ARCHIVO TEMPORAL
+
+        //CONDICION PARA VERIFICAR QUE EXISTE UNA IMAGEN
         if($check !== false) {
-            //echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            //echo "File is not an image.";
             $uploadOk = 0;
         }
 
         if ($uploadOk == 0) {
             return "";
-            //echo "Sorry, your file was not uploaded.";
-            //$this->redirect('user', ['error' => Errors::ERROR_USER_UPDATEPHOTO_FORMAT]);
-        // if everything is ok, try to upload file
         } else {
-            if (move_uploaded_file($photo["tmp_name"], $target_file)) {
-                return $hash;
+            if (move_uploaded_file($photo["tmp_name"], $target_file)) {//MUEVE LA IMGANE DESDE ARCHIVOS TEMPORALES A LA NUEVA RUTA
+                return $hash; //RETORNA EL HASH QUE SE GUARDA EN LA BD
             } else {
                 return '';
             }
