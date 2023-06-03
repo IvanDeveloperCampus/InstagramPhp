5eb5a5d7c18459ce9e4f169abf972c8f2c443b42
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-03 15:33:24 -0500

    feat: :sparkles: Modelo PostImage
    PostImage es para post de tipo Post Image ya que ´peuden a ver varios tipos de post

--- src/models/PostImage.php
+++ src/models/PostImage.php
@@ -6,6 +6,16 @@ use Skylab170\InstagramPhp\models\Post;
 
 class POstImage extends Post{
 
+    public function __construct(private string $title, private string $image){
+        parent::__construct($title);
+    }
+
+    public function getImage(){
+        return $this->image;
+    }
+
+    
+
 }
 
 ?>
