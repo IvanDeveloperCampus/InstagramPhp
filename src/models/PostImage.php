40a4b3123a8d945444fa960d0e845a8fcfb6b411
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-03 17:04:03 -0500

    feat: :sparkles: Funcion getFeed para retornar todos los posts
    funcion para mostrar en home, todas las publicaciones y su informacion correspondiente

--- src/models/PostImage.php
+++ src/models/PostImage.php
@@ -3,6 +3,9 @@
 namespace Skylab170\InstagramPhp\models;
 
 use Skylab170\InstagramPhp\models\Post;
+use Skylab170\InstagramPhp\lib\Database;
+use PDO;
+use PDOException;
 
 class POstImage extends Post{
 
@@ -13,6 +16,32 @@ class POstImage extends Post{
     public function getImage(){
         return $this->image;
     }
+
+    public static function getFeed(){
+        $items=[];
+        try {
+            $db=new Database();
+            $query=$db->connect()->query('SELECT * FROM posts ORDER BY post_id DESC');
+            
+            while ($p=$query->fetch(PDO::FETCH_ASSOC)) {
+                $item=new POstImage($p['title'], $p['media']);//se crea un nuevo objeto imagen
+                $item->setId($p['post_id']);//se le asigna el id del post
+                $item->fetchLikes();//para traer los likes de cada post
+                $user=User::getById($p['user_id']);//se trae el usuario del post
+                $item->setUser($user);//se el asigna al post
+                array_push($items, $item);
+            }
+            
+            return $items;
+
+        } catch (PDOException $th) {
+            echo $th;
+        }
+    }
+
+    
+
+
 
     
 
