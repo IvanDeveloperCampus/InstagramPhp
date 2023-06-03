9dce564768a6b9b588a1fb75a4424c8880df3e6c
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-03 15:10:43 -0500

    feat: :sparkles: Modelo like
    Se implemento el metodo save

--- src/models/Like.php
+++ src/models/Like.php
@@ -3,6 +3,7 @@
 
 namespace Skylab170\InstagramPhp\models;
 
+use PDOException;
 use Skylab170\InstagramPhp\lib\Model;
 
 
@@ -10,16 +11,30 @@ class Like extends Model{
     
     private int $id;
 
-    public function __construct(private int $post_id){
+
+    public function __construct(private int $post_id, private int $user_id){
         parent::__construct();
     }
 
     public function save(){
-
+        try {
+            $query=$this->prepare('INSERT INTO likes (post_id, user_id) VALUES (:post_id,:user_id');
+            $query->execute([
+                'post_id'=>$this->post_id,
+                'user_id'=>$this->user_id
+            ]);
+            return true;
+        } catch (PDOException $th) {
+            return false;
+        }
     }
 
     public function setId($value){
         $this->id=$value;
+    }
+
+    public function setUserId($value){
+        $this->user_id=$value;
     }
 
     public function getId(){
