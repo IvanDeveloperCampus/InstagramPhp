7a8b4d2dafbc6a366d0370f06eee19bc167d486d
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-03 18:52:32 -0500

    feat: :sparkles: Clase para implementar el like
    Se implemento el metodo de dar like ademas aqui se puede agergar comentarios y dar like a comentarios

---
+++ src/controllers/Actions.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Skylab170\InstagramPhp\controllers;
+
+use Skylab170\InstagramPhp\lib\Controller;
+use Skylab170\InstagramPhp\models\POstImage;
+use Skylab170\InstagramPhp\Models\User;
+
+class Actions extends Controller{
+    public function __construct(private User $user){
+        parent::__construct();
+    }
+
+    public function like(){
+        //recoge el id del post que se envia desde la vista
+        $post_id=$this->post('post_id');
+        $origin=$this->post('origin');
+
+        if (!is_null($post_id) && !is_null($origin)) {
+            $post=POstImage::get($post_id);//se trae el post segun el id especifico
+            $post->addLike($this->user);
+            header('location: /instagramPhp/' . $origin);
+            
+        }
+    }
+}
