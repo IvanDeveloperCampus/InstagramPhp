4c212a444b675727cfa1de541e3ce34b006b037d
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-04 17:16:45 -0500

    chore: :fire: Elimine componente de create ya no era necesario

--- src/components/create.php
+++
@@ -1,9 +0,0 @@
-<div class="create">
-    <form action="publish" method="POST" enctype="multipart/form-data">
-        <textarea name="title" rows="3"></textarea>
-        <div>
-            <input type="file"  name="image" accept="image/png">
-            <input type="submit" value="Publicar">
-        </div>
-    </form>
-</div>
