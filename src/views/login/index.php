316b066cd4dffc2b6db0f6707891d98af45b7543
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-04 17:59:43 -0500

    style: :art: Interfaz mejorada del modulo de login y registro

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/Assets/css/styleLogin.css">
     <title>Document</title>
 </head>
+
 <body>
 
-    <form action="auth" method="POST">
-        <input type="text" name="username">
-        <input type="text" name="password">
-        <input type="submit" value="login">
-    </form>
-    
+    <div class="container">
+        <div class="center">
+            <div class="header">
+                <img src="src/assets/img/logo.PNG" alt="instagramLogo" class="instaLogo" />
+            </div>
+            <form action="auth" method="POST">
+                <div class="inputElement">
+                    
+                        <input type="text" placeholder="Username" name="username" class="inputText" />
+                        <input type="password" placeholder="Password"  name="password" class="inputText" />
+                        <input type="submit" value="Log in" class="inputButton" />
+                 
+                    <div class="line">
+                        <span class="arrow"></span>
+                        <span class="content">OR</span>
+                        <span class="arrow"></span>
+                    </div>
+                    <div class="social__icon">
+                        <i class="fab fa-facebook-square"></i><span>Log in with facebook</span>
+                    </div>
+                    <div class="forgetPassword">Forget Password?</div>
+                </div>
+            </form>
+        </div>
+        <div class="footer">
+            <p>Don't have a accout?<a href="/InstagramPhp/signup">Sign Up</a></p>
+        </div>
+    </div>
+
 </body>
+
 </html>
--- src/views/signup/index.php
+++ src/views/signup/index.php
@@ -4,16 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/Assets/css/styleLogin.css">
     <title>Document</title>
 </head>
 <body>
 
-    <form action="register" method="POST" enctype="multipart/form-data">
-        <input type="text" name="username">
-        <input type="text" name="password">
-        <input type="file" name="profile">
-        <input type="submit" value="Crear Cuenta">
-    </form>
+
+    <div class="container">
+        <div class="center">
+            <div class="header">
+                <img src="src/assets/img/logo.PNG" alt="instagramLogo" class="instaLogo" />
+            </div>
+            <form action="register" method="POST">
+                <div class="inputElement">
+                    
+                        <input type="text" placeholder="Username" name="username" class="inputText" />
+                        <input type="password" placeholder="Password"  name="password" class="inputText" />
+                        <input type="file" name="profile">
+                        <input type="submit" value="Log in" class="inputButton" />
+                 
+                    <div class="line">
+                        <span class="arrow"></span>
+                        <span class="content">OR</span>
+                        <span class="arrow"></span>
+                    </div>
+                    <div class="social__icon">
+                        <i class="fab fa-facebook-square"></i><span>Sign Up with facebook</span>
+                    </div>
+            
+                </div>
+            </form>
+        </div>
+        <div class="footer">
+            <p>Do you have a accout?<a href="/InstagramPhp/login">Log In</a></p>
+        </div>
+    </div>
     
 </body>
 </html>
