50d7fa5dd8314db48a1b88909eaec233cb9a9a2c
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-05 23:01:36 -0500

    feat: :sparkles: Metodo para verificar si ya existe el like del usuario

--- src/models/Like.php
+++ src/models/Like.php
@@ -7,48 +7,74 @@ use PDOException;
 use Skylab170\InstagramPhp\lib\Model;
 
 
-class Like extends Model{
-    
+class Like extends Model
+{
+
     private int $id;
 
 
-    public function __construct(private int $post_id, private int $user_id){
+    public function __construct(private int $post_id, private int $user_id)
+    {
         parent::__construct();
     }
 
-    public function save(){
-        
+    public function save()
+    {
+
         try {
-            
+
             $query = $this->prepare('INSERT INTO likes (post_id, user_id) VALUES(:post_id, :user_id)');
             $query->execute([
-                'post_id'=>$this->post_id,
-                'user_id'=>$this->user_id
+                'post_id' => $this->post_id,
+                'user_id' => $this->user_id
             ]);
 
             return true;
         } catch (PDOException $th) {
-            
+
             echo $th->getMessage();
         }
     }
 
-    public function setId($value){
-        $this->id=$value;
+    public function existsLike()
+    {
+        try {
+            $query = $this->prepare('SELECT COUNT(*) FROM likes WHERE post_id=:post_id AND user_id=:user_id;');
+            $query->execute([
+                'post_id' => $this->post_id,
+                'user_id' => $this->user_id
+            ]);
+            if ($query->fetchColumn() < 1) {
+                $this->save();
+            } else {
+                $deleteQuery = $this->prepare('DELETE FROM likes WHERE post_id=:post_id AND user_id=:user_id;');
+                $deleteQuery->execute([
+                    'post_id' => $this->post_id,
+                    'user_id' => $this->user_id
+                ]);
+            }
+        } catch (\PDOException $th) {
+            echo $th->getMessage();
+        }
     }
 
-    public function setUserId($value){
-        $this->user_id=$value;
+    public function setId($value)
+    {
+        $this->id = $value;
     }
 
-    public function getId(){
+    public function setUserId($value)
+    {
+        $this->user_id = $value;
+    }
+
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getPostId(){
+    public function getPostId()
+    {
         return $this->post_id;
     }
-
 }
-
-?>
