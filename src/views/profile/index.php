9ea0c8f2eb166593af807c9af2d60e11c9606522
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-13 18:19:52 -0500

    feat: :sparkles: Modales de seguidores y seguidos

--- src/views/profile/index.php
+++ src/views/profile/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
     <title>Document</title>
     <link rel="stylesheet" href="src/Assets/css/styleProfile.css">
 </head>
@@ -36,15 +37,22 @@
                     </form>
                 </div>
                 <div class="cabezera2">
-                    <p><b><?php echo $user->countGetPosts() ?></b> publicaciones</p>                
-                    <p class="seguidores"><b><?php echo $user->countGetFollowers() ?></b> seguidores</p>
-                    <p class="seguidos"><b><?php echo $user->countGetFollowed() ?></b> seguidos</p>
+                    <p><b><?php echo $user->countGetPosts() ?></b> publicaciones</p>
+                    <a  data-bs-toggle="modal" data-bs-target="#myModalFollowers">
+                        <p class="seguidores"><b><?php echo $user->countGetFollowers() ?></b> seguidores</p>
+                    </a>
+                    <a  data-bs-toggle="modal" data-bs-target="#myModalFollowed">
+                        <p class="seguidos"><b><?php echo $user->countGetFollowed() ?></b> seguidos</p>
+                    </a>
                 </div>
                 <div class="cabezera3">
                     <!--informacion del perfil-->
                 </div>
             </div>
         </div>
+
+
+        
 
         <div class="historias">
 
@@ -75,7 +83,45 @@
     </main>
 
 
+    <!--MODALL-->
+    <div class="modal fade" id="myModalFollowers" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 text-center" id="exampleModalLabel">Followers</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <?php
+                        foreach ($user->getFollowers() as $seguidores) { ?>
+                            <p><?php echo $seguidores ?></p>
+                        <?php  }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="myModalFollowed" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5 text-center" id="exampleModalLabel">Followers</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <?php
+                        foreach ($user->getFollowed() as $seguidos) { ?>
+                            <p><?php echo $seguidos ?></p>
+                        <?php  }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
 
 </html>
