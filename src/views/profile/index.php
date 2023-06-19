1affff2ca41bb69f2c87488cf72bf8492f4d4220
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-19 12:31:40 -0500

    style: :art: Diseño para desktop
    falta adaptar a movil

--- src/views/profile/index.php
+++ src/views/profile/index.php
@@ -22,7 +22,7 @@
     ?>
 
     <header class="mt-5">
-        <div class="perfil-container">
+        <div class="container perfil-container">
             <img class="img-perfil" src="public/img/photos/<?php echo $user->getProfile() ?>" alt="logo-perfil" width="10%">
             <div class="contenedor-cabezera">
                 <div class="cabezera1">
@@ -42,7 +42,7 @@
                        <?php }
                         ?>
                         
-                        <svg aria-label="Opciones" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24">
+                        <svg aria-label="Opciones ml-3" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24">
                             <path clip-rule="evenodd" d="M46.7 20.6l-2.1-1.1c-.4-.2-.7-.5-.8-1-.5-1.6-1.1-3.2-1.9-4.7-.2-.4-.3-.8-.1-1.2l.8-2.3c.2-.5 0-1.1-.4-1.5l-2.9-2.9c-.4-.4-1-.5-1.5-.4l-2.3.8c-.4.1-.8.1-1.2-.1-1.4-.8-3-1.5-4.6-1.9-.4-.1-.8-.4-1-.8l-1.1-2.2c-.3-.5-.8-.8-1.3-.8h-4.1c-.6 0-1.1.3-1.3.8l-1.1 2.2c-.2.4-.5.7-1 .8-1.6.5-3.2 1.1-4.6 1.9-.4.2-.8.3-1.2.1l-2.3-.8c-.5-.2-1.1 0-1.5.4L5.9 8.8c-.4.4-.5 1-.4 1.5l.8 2.3c.1.4.1.8-.1 1.2-.8 1.5-1.5 3-1.9 4.7-.1.4-.4.8-.8 1l-2.1 1.1c-.5.3-.8.8-.8 1.3V26c0 .6.3 1.1.8 1.3l2.1 1.1c.4.2.7.5.8 1 .5 1.6 1.1 3.2 1.9 4.7.2.4.3.8.1 1.2l-.8 2.3c-.2.5 0 1.1.4 1.5L8.8 42c.4.4 1 .5 1.5.4l2.3-.8c.4-.1.8-.1 1.2.1 1.4.8 3 1.5 4.6 1.9.4.1.8.4 1 .8l1.1 2.2c.3.5.8.8 1.3.8h4.1c.6 0 1.1-.3 1.3-.8l1.1-2.2c.2-.4.5-.7 1-.8 1.6-.5 3.2-1.1 4.6-1.9.4-.2.8-.3 1.2-.1l2.3.8c.5.2 1.1 0 1.5-.4l2.9-2.9c.4-.4.5-1 .4-1.5l-.8-2.3c-.1-.4-.1-.8.1-1.2.8-1.5 1.5-3 1.9-4.7.1-.4.4-.8.8-1l2.1-1.1c.5-.3.8-.8.8-1.3v-4.1c.4-.5.1-1.1-.4-1.3zM24 41.5c-9.7 0-17.5-7.8-17.5-17.5S14.3 6.5 24 6.5 41.5 14.3 41.5 24 33.7 41.5 24 41.5z" fill-rule="evenodd"></path>
                         </svg>
                     </form>
@@ -62,21 +62,12 @@
             </div>
         </div>
 
-
-
-
-        <div class="historias">
-
-        </div>
     </header>
-    <main>
+    <main class="container">
         <div class="linea"></div>
         <div class="main-nav">
             <a href="#">
                 PUBLICACIONES
-            </a>
-            <a href="#">
-                IGTV
             </a>
             <a href="#">
                 GUARDADAS
@@ -85,7 +76,7 @@
                 ETIQUETADAS
             </a>
         </div>
-        <div class="mainCont__grid">
+        <div class="container mainCont__grid">
             <?php
             foreach ($user->getPosts() as $p) { ?>
                 <img src="public/img/photos/<?php echo $p->getImage() ?>" width="100%" />
@@ -129,6 +120,8 @@
             </div>
         </div>
     </div>
+
+    <?php require_once("src/views/Layouts/footer.php") ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
