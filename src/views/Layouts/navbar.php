3292727c297b08762ea965de7a9f20de97abd19e
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-21 14:44:39 -0500

    style: :art: Diseño responsive e imagenes nueva

--- src/views/Layouts/navbar.php
+++ src/views/Layouts/navbar.php
@@ -1,89 +1,97 @@
 <nav class="navbar">
-        <div class="nav-wrapper">
-            <img src="src/assets/img/logo.PNG" class="ins-img" alt="">
-            <input type="text" class="search-box" placeholder="search">
-            <div class="nav-items">
-                <img src="src/assets/img/home.PNG" class="icon" alt="">
-                <img src="src/assets/img/messenger.PNG" class="icon" alt="">
-                <img data-bs-toggle="modal" data-bs-target="#post-add-modal" src="src/assets/img/add.PNG" class="icon" alt="">
-                <img src="src/assets/img/explore.PNG" class="icon" alt="">
-                <img src="src/assets/img/like.PNG" class="icon" alt="">
-                <a class="link-dark" href="/instagramPhp/<?php echo $user->getUsername() ?>">
-                    <img class="rounded-circle" src="public/img/photos/<?php echo $user->getProfile() ?>" width="22" />
-                </a>
+    <div class="nav-wrapper">
+        <img src="src/assets/img/logo.PNG" class="ins-img" alt="">
+        <input type="text" class="search-box" placeholder="search">
+        <div class="nav-items">
+            <img src="src/assets/img/home.PNG" class="icon" alt="">
+            <img src="src/assets/img/messenger.PNG" class="icon" alt="">
+            <img data-bs-toggle="modal" data-bs-target="#post-add-modal" src="src/assets/img/add.PNG" class="icon" alt="">
+            <img src="src/assets/img/explore.PNG" class="icon" alt="">
+            <img src="src/assets/img/like.PNG" class="icon" alt="">
+            <a class="link-dark" href="/instagramPhp/<?php echo $user->getUsername() ?>">
+                <img class="rounded-circle" src="public/img/photos/<?php echo $user->getProfile() ?>" width="22" />
+            </a>
 
-            </div>
         </div>
+    </div>
 </nav>
 
 <style>
     /* Navbar Start */
-.navbar{
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 50px;
-    background: #fff;
-    border-bottom: 1px solid #dfdfdf;
-    display: flex;
-    justify-content: center;
-    padding: 5px 0;
-}
+    .navbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 50px;
+        background: #fff;
+        border-bottom: 1px solid #dfdfdf;
+        display: flex;
+        justify-content: center;
+        padding: 5px 0;
+    }
 
-.nav-wrapper{
-    width: 70%;
-    max-width: 1344px;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
+    .nav-wrapper {
+        width: 70%;
+        max-width: 1344px;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-.ins-img{
-    height: 100%;
-    margin-top: 7px;
-    margin-left: 230px;
-}
+    .ins-img {
+        height: 100%;
+        margin-top: 7px;
+        margin-left: 200px;
+    }
 
-.search-box{
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 200px;
-    height: 25px;
-    background: #fafafa;
-    border: 1px solid #dfdfdf;
-    border-radius: 2px;
-    color: rgba(0, 0, 0, 0.5);
-    text-align: center;
-    text-transform: capitalize;
-    margin-top: 7px;
-}
+    .search-box {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 200px;
+        height: 25px;
+        background: #fafafa;
+        border: 1px solid #dfdfdf;
+        border-radius: 2px;
+        color: rgba(0, 0, 0, 0.5);
+        text-align: center;
+        text-transform: capitalize;
+        margin-top: 7px;
+    }
 
-.search-box::placeholder{
-    color: rgba(0, 0, 0, 0.5);
-}
+    .search-box::placeholder {
+        color: rgba(0, 0, 0, 0.5);
+    }
 
-.nav-items{
-    height: 22px;
-    position: relative;
-    margin-right: -65px;
-}
+    .nav-items {
+        height: 22px;
+        position: relative;
+        margin-right: -65px;
+    }
 
-.icon{
-    height: 100%;
-    cursor: pointer;
-    margin: -7px 10px;
-    display: inline-block;
-}
+    .icon {
+        height: 100%;
+        cursor: pointer;
+        margin: -7px 10px;
+        display: inline-block;
+    }
 
-@media screen and (max-width: 576px) {
+    @media screen and (max-width: 576px) {
 
-    .nav-items{
-    display: none;
-}
-}
+        .nav-items {
+            display: none;
+        }
 
-/* Navbar End */
+        .search-box {
+            display: none;
+        }
+
+        .ins-img {
+            margin-left: -50px;
+        }
+    }
+
+    /* Navbar End */
 </style>
--- src/views/profile/index.php
+++ src/views/profile/index.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
     <link rel="stylesheet" href="src/Assets/css/styleProfile.css">
 </head>
@@ -21,7 +20,7 @@
     require_once("src/views/Layouts/navbar.php");
     ?>
 
-    <header class="mt-5">
+    <header class="mt-5 mb-5">
         <div class="container perfil-container">
             <img class="img-perfil" src="public/img/photos/<?php echo $user->getProfile() ?>" alt="logo-perfil" width="10%">
             <div class="contenedor-cabezera">
@@ -37,11 +36,11 @@
                             <button class="btn btn-primary seguir">Seguir</button>
                             <button class="sendMessage">Enviar Mensaje</button>
                         <?php
-                        }else{?>
+                        } else { ?>
                             <button class="sendMessage">Editar perfil</button>
-                       <?php }
+                        <?php }
                         ?>
-                        
+
                         <svg aria-label="Opciones ml-3" class="_8-yf5 " fill="#262626" height="24" viewBox="0 0 48 48" width="24">
                             <path clip-rule="evenodd" d="M46.7 20.6l-2.1-1.1c-.4-.2-.7-.5-.8-1-.5-1.6-1.1-3.2-1.9-4.7-.2-.4-.3-.8-.1-1.2l.8-2.3c.2-.5 0-1.1-.4-1.5l-2.9-2.9c-.4-.4-1-.5-1.5-.4l-2.3.8c-.4.1-.8.1-1.2-.1-1.4-.8-3-1.5-4.6-1.9-.4-.1-.8-.4-1-.8l-1.1-2.2c-.3-.5-.8-.8-1.3-.8h-4.1c-.6 0-1.1.3-1.3.8l-1.1 2.2c-.2.4-.5.7-1 .8-1.6.5-3.2 1.1-4.6 1.9-.4.2-.8.3-1.2.1l-2.3-.8c-.5-.2-1.1 0-1.5.4L5.9 8.8c-.4.4-.5 1-.4 1.5l.8 2.3c.1.4.1.8-.1 1.2-.8 1.5-1.5 3-1.9 4.7-.1.4-.4.8-.8 1l-2.1 1.1c-.5.3-.8.8-.8 1.3V26c0 .6.3 1.1.8 1.3l2.1 1.1c.4.2.7.5.8 1 .5 1.6 1.1 3.2 1.9 4.7.2.4.3.8.1 1.2l-.8 2.3c-.2.5 0 1.1.4 1.5L8.8 42c.4.4 1 .5 1.5.4l2.3-.8c.4-.1.8-.1 1.2.1 1.4.8 3 1.5 4.6 1.9.4.1.8.4 1 .8l1.1 2.2c.3.5.8.8 1.3.8h4.1c.6 0 1.1-.3 1.3-.8l1.1-2.2c.2-.4.5-.7 1-.8 1.6-.5 3.2-1.1 4.6-1.9.4-.2.8-.3 1.2-.1l2.3.8c.5.2 1.1 0 1.5-.4l2.9-2.9c.4-.4.5-1 .4-1.5l-.8-2.3c-.1-.4-.1-.8.1-1.2.8-1.5 1.5-3 1.9-4.7.1-.4.4-.8.8-1l2.1-1.1c.5-.3.8-.8.8-1.3v-4.1c.4-.5.1-1.1-.4-1.3zM24 41.5c-9.7 0-17.5-7.8-17.5-17.5S14.3 6.5 24 6.5 41.5 14.3 41.5 24 33.7 41.5 24 41.5z" fill-rule="evenodd"></path>
                         </svg>
@@ -61,29 +60,31 @@
                 </div>
             </div>
         </div>
-
     </header>
-    <main class="container">
-        <div class="linea"></div>
-        <div class="main-nav">
-            <a href="#">
-                PUBLICACIONES
-            </a>
-            <a href="#">
-                GUARDADAS
-            </a>
-            <a href="#">
-                ETIQUETADAS
-            </a>
+    <section class="mt-5">
+        <div class="container publicaciones">
+            <div class="linea"></div>
+            <div class="main-nav">
+                <a href="#">
+                    PUBLICACIONES
+                </a>
+                <a href="#">
+                    GUARDADAS
+                </a>
+                <a href="#">
+                    ETIQUETADAS
+                </a>
+            </div>
+            <div class="container mainCont__grid">
+                <?php
+                foreach ($user->getPosts() as $p) { ?>
+                    <img  class="item" src="public/img/photos/<?php echo $p->getImage() ?>" width="100%" />
+                <?php } ?>
+            </div>
         </div>
-        <div class="container mainCont__grid">
-            <?php
-            foreach ($user->getPosts() as $p) { ?>
-                <img src="public/img/photos/<?php echo $p->getImage() ?>" width="100%" />
-            <?php } ?>
-        </div>
-    </main>
 
+    </section>
+    <?php require_once("src/views/Layouts/footer.php") ?>
 
     <!--MODALL-->
     <div class="modal fade" id="myModalFollowers" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -121,11 +122,11 @@
         </div>
     </div>
 
-    <?php require_once("src/views/Layouts/footer.php") ?>
+    
 
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 
 </html>
